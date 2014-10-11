2774904d5d6bad215f8461b48b7d8effd3989a38
tholorthored <mul_1@web.de>
2014-10-11 09:21:54 +0200

    minor changes in load function with ff-package

--- loadFunctions.R
+++ loadFunctions.R
@@ -17,30 +17,27 @@ testFileNames = fileNames[grepl("+test+",fileNames)]
 
 
 ###Load Data
-#change "20" with dynamic statement later!
+#change "20" with dynamic statement "length(files)" later!
 loadData = function(path, files){
   data = vector(mode="list", length=20)
   for(i in 1:20){
     filePath = paste0(path,files[i])
     temp = readMat(filePath)
-    #foo = ff(files[i], vmode = "short",dim = dim(temp[[1]][1][[1]]))
     data[[i]] = temp[[1]][1][[1]]
-    #foo = temp[[1]][1][[1]]
   }
   names(data) = files[1:20]
-  #ff(data)
   return(data)
 }
 
+##Load function incorporating the ff-package for large data sets
+# not working yet
 loadBigData = function(path, files){
-  for(i in 1:20){
+  for(i in 1:2){
     filePath = paste0(path,files[i])
-    temp = as.ff(readMat(filePath))
-    #foo = ff(files[i], vmode = "short",dim = dim(temp[[1]][1][[1]]))
-    data[[i]] = temp[[1]][1][[1]]
-    #foo = temp[[1]][1][[1]]
+    temp = readMat(filePath)
+    foo = ff( vmode = "short",dim = dim(temp[[1]][1][[1]]))
+    foo = temp[[1]][1][[1]]
   }
-  names(data) = files[1:20]
-  #ff(data)
-  return(data)
+  #names(foo) = files[1:2]
+  return(foo)
 }
