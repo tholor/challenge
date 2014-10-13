61dcce5af88c50a523181ca0aa653111edd65814
Tom Gleich <tom@live.de>
2014-10-13 12:47:25 +0200

    cosmetic changes to test RStudio integration

--- loadFunctions.R
+++ loadFunctions.R
@@ -1,23 +1,23 @@
 
 
-#get all the correct filenames from the path
+# get all the correct filenames from the path
 fileNames = list.files(path)
-#only filenames with the targets in it 
+# only filenames with the targets in it 
 targetString = ""
 for (i in 1:(length(targets)-1)){
   print(i)
   targetString = paste0(targetString,targets[i],"|")
 }
 paste0(targetString,targets[i+1])
-#split the filenames according to seizure period
+# split the filenames according to seizure period
 fileNames = fileNames[grepl(paste0("+(",targets[1],"|",targets[2],")+"),fileNames)]
 interictalFileNames = fileNames[grepl("+interictal+",fileNames)]
 preictalFileNames = fileNames[grepl("+preictal+",fileNames)]
 testFileNames = fileNames[grepl("+test+",fileNames)]
 
 
-###Load Data
-#change "20" with dynamic statement "length(files)" later!
+## Load Data
+# change "20" with dynamic statement "length(files)" later!
 loadData = function(path, files){
   data = vector(mode="list", length=20)
   for(i in 1:20){
@@ -29,20 +29,20 @@ loadData = function(path, files){
   return(data)
 }
 
-##Load function incorporating the ff-package for large data sets
+## Load function incorporating the ff-package for large data sets
 # not working yet
 loadBigFiles = function(path, files){
   for(i in 1:2){
     assign (paste0(big,file[i]), loadSingleBigFile)
   }
-  # assign all the variable
+  # assign all variables
   return(bigFile)
 }
 
-#loading a single .mat file into a ff file
+# loading a single .mat file into a ff file
 loadSingleBigFile = function (filepath){
   temp = readMat(filepath)
-  #bigFile = ff(vmode = "short",dim = dim(temp[[1]][1][[1]]))
+  # bigFile = ff(vmode = "short",dim = dim(temp[[1]][1][[1]]))
   bigFile = temp[[1]][1][[1]]
   rm(temp)
   gc()
